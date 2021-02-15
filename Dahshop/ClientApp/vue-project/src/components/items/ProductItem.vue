f81e33c9a600062ad71f374982ba888838532ff5
Matthew Ling <matthew.ling98@gmail.com>
2021-02-15 23:54:05 +0100

    image display works dynamically and with buttons switching image

--- Dahshop/ClientApp/vue-project/src/components/items/ProductItem.vue
+++ Dahshop/ClientApp/vue-project/src/components/items/ProductItem.vue
@@ -1,7 +1,29 @@
 <template>
     <div>
         <li>
+          <div class="text-center">
+
             <h3>{{ name }}</h3>
+
+            <div class="container">
+              <div class="row justify-content-md-center">
+                <div class="col-md-auto">
+                  <img :src="setSelectedImage(selectedIndex)" width="300" height="375">
+                </div>
+              </div>
+
+              <div class="col" v-if="imageList.length > 1">
+                  <button @click="choosePrevImage">
+                    <img src="images\arrow-left.png" alt="left" width="40" height="35">
+                  </button>
+                  <button @click="chooseNextImage">
+                    <img src="images\arrow-right.png" alt="right" width="40" height="35">
+                  </button>
+                </div>
+
+
+            </div>
+
             <p>Color: {{ color }}</p>
             <p>Size: {{ size }}</p>
             <p>Location: {{ location }}</p>
@@ -10,15 +32,68 @@
             <div>
                 <base-button mode="outline">View details</base-button>
             </div>
+
+          </div>
+            
         </li>
     </div>
 </template>
 
 <script>
+
+import {ref} from 'vue';
+
 export default {
-    props: ['id', 'name', 'color', 'size', 'price', 'location', 'description'],
-    computed: {
-        //....
+    props: ['id', 'name', 'color', 'size', 'price', 'location', 'description', "filePath"],
+    setup(props){
+      
+      // Split the filepaths into an array of image files
+      const splitFilepath = props.filePath.split(',');
+      console.log(splitFilepath);
+
+      // Store the array 
+      const imageList = ref(splitFilepath);
+
+      console.log(imageList.value.length);
+
+      // Variable to store selected index of array
+      const selectedIndex = ref(0);
+
+      // Variable to use as source for the selected image
+      const img_src = ref("");
+
+      // Choose the prev image
+      function choosePrevImage(){
+        if(selectedIndex.value != 0){
+          // console.log("Current index: " + selectedIndex.value);
+          selectedIndex.value--;
+          console.log("Previous image chosen! Index: " + selectedIndex.value);
+          // setSelectedImage();
+        }
+      }
+
+      // Choose the next image
+      function chooseNextImage(){
+        if(selectedIndex.value != imageList.value.length-1){
+          // console.log("Current index: " + selectedIndex.value);
+          selectedIndex.value++;
+          console.log("Next image chosen! Index: " + selectedIndex.value);
+          // setSelectedImage();
+        }
+      }
+
+      // Set the image file based on index
+      function setSelectedImage(index){
+        console.log("Selected image file " + imageList.value[index]);
+        return imageList.value[index];
+      }
+
+      // const getImage = computed(function() {
+      //    return img_src.value;
+      // });
+
+      return { imageList, img_src, selectedIndex, choosePrevImage, chooseNextImage, setSelectedImage }
+
     }
 }
 </script>
